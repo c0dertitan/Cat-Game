f8eb9373d432f8709fa18314d987a7eae72e19c6
Predator Software <acclimate.kite198@addymail.com>
2022-05-01 14:42:43 -0500

    All apart of the cat game. @c0dertitan

---
+++ _javascript/avatar.js
@@ -0,0 +1,4 @@
+// Avatar Content
+
+console.log('Make sure it can change color.')
+alert('Do you want to change your color?')
---
+++ _javascript/credits.js
@@ -0,0 +1,4 @@
+// These are the credits.
+
+console.log('1: c0dertitan 2: willywallabean')
+alert('These are the creators Credit to c0dertitan for making the game Credit to willywallabean for coming up with it.')
